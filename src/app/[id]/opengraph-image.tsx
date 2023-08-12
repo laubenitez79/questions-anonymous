4f52a3ea67976dce785af18d31b3ea11bc1dd2fb
laubenitez79 <laubenitez@uade.edu.ar>
2023-08-12 05:53:14 -0300

    Se agrega opengraph-image, ruta para cada pregunta

--- src/app/[id]/opengraph-image.tsx
+++ src/app/[id]/opengraph-image.tsx
@@ -1,3 +1,5 @@
+import type {GetStaticPaths, GetStaticProps} from "next";
+
 import {createClient} from "@supabase/supabase-js";
 import {ImageResponse} from "next/server";
 
@@ -6,62 +8,91 @@ const supabase = createClient(
   process.env.SUPABASE_KEY!,
 );
 
-export default async function Image({params: {id}}: {params: {id: string}}) {
+export default function Image({question}: {question: {id: string; text: string}}) {
+  return (
+    <div
+      style={{
+        fontSize: 64,
+        background: "black",
+        color: "white",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 auto",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "hotpink",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p>Questioncy</p>
+      </div>
+      <div
+        style={{
+          backgroundColor: "white",
+          color: "black",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p
+          style={{
+            textAlign: "center",
+          }}
+        >
+          {question.text}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const {data, error} = await supabase.from("questions").select("id");
+
+  if (error) {
+    console.error(error);
+
+    return {
+      paths: [],
+      fallback: false,
+    };
+  }
+
+  const paths = data.map((question: {id: string}) => ({
+    params: {id: question.id},
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const {id} = params!;
   const {data, error} = await supabase.from("questions").select().eq("id", id).single();
 
   if (error) {
-    // Manejo del error
+    console.error(error);
+
+    return {
+      notFound: true,
+    };
   }
 
   const question = data as {id: string; text: string};
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 64,
-          background: "black",
-          color: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          margin: "0 auto",
-        }}
-      >
-        <div
-          style={{
-            backgroundColor: "hotpink",
-            color: "white",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <p>Questioncy</p>
-        </div>
-        <div
-          style={{
-            backgroundColor: "white",
-            color: "black",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <p
-            style={{
-              textAlign: "center",
-            }}
-          >
-            {question.text}
-          </p>
-        </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
+  return {
+    props: {
+      question,
     },
-  );
-}
+  };
+};
