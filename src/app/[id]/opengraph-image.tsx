374d404dc2e602f19fefce5427c1725a56e709af
laubenitez79 <laubenitez@uade.edu.ar>
2023-08-12 05:51:34 -0300

    Se agrega opengraph-image, ruta para cada pregunta

--- src/app/[id]/opengraph-image.tsx
+++ src/app/[id]/opengraph-image.tsx
@@ -1,24 +1,19 @@
 import {createClient} from "@supabase/supabase-js";
 import {ImageResponse} from "next/server";
 
-// Route segment config
-export const runtime = "edge";
-
-export const contentType = "image/png";
-
 const supabase = createClient(
   "https://kzaxahkhotoyvmujnsut.supabase.co",
   process.env.SUPABASE_KEY!,
 );
 
-// Image generation
 export default async function Image({params: {id}}: {params: {id: string}}) {
-  const question = await supabase
-    .from("questions")
-    .select()
-    .eq("id", id)
-    .single()
-    .then(({data}) => data as {id: string; text: string});
+  const {data, error} = await supabase.from("questions").select().eq("id", id).single();
+
+  if (error) {
+    // Manejo del error
+  }
+
+  const question = data as {id: string; text: string};
 
   return new ImageResponse(
     (
