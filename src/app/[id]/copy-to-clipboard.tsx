317c867f486a0c9a98c9c8b92b53da14de792e41
laubenitez79 <laubenitez@uade.edu.ar>
2023-08-15 02:01:45 -0300

    Estilos nuevos

--- src/app/[id]/copy-to-clipboard.tsx
+++ src/app/[id]/copy-to-clipboard.tsx
@@ -1,7 +1,17 @@
 "use client";
+import React, {useState} from "react";
 import Swal from "sweetalert2";
 
 export default function CopyQuestionToClipboard() {
+  const [showEmojis, setShowEmojis] = useState(false);
+
+  // async function shareToInstagramStories(imageUrl, caption) {
+  //   const encodedCaption = encodeURIComponent(caption);
+  //   const instagramUrl = `https://www.instagram.com/stories/create/?backgroundImage=${imageUrl}&text=${encodedCaption}`;
+
+  //   window.location.href = instagramUrl;
+  // }
+
   async function handleClick() {
     const image = await fetch(`${location.pathname}/opengraph-image`).then((res) => res.blob());
 
@@ -11,22 +21,129 @@ export default function CopyQuestionToClipboard() {
       }),
     ]);
 
-    Swal.fire("PreguntaXpress", "Copiado en portapapeles", "success")
-      .then((res) => {
-        res;
-      })
-      .catch((err) => {
-        err;
-      });
+    setShowEmojis(true);
+
+    setTimeout(() => {
+      setShowEmojis(false);
+    }, 900);
+
+    // Swal.fire("PreguntaXpress", "Copiado en portapapeles", "success")
+    //   .then((res) => {
+    //     res;
+    //   })
+    //   .catch((err) => {
+    //     err;
+    //   });
   }
 
   return (
-    <button
-      className="transition-color w-full rounded-lg bg-orange-500 p-4 text-xl font-medium hover:bg-orange-600"
-      type="button"
-      onClick={handleClick}
-    >
-      Copiar al portapapeles
-    </button>
+    <div className="grid">
+      <button
+        className="transition-color w-full rounded-lg bg-orange-500 p-4 text-xl font-medium hover:bg-orange-400"
+        type="button"
+        onClick={handleClick}
+      >
+        Copiar al portapapeles
+      </button>
+      {showEmojis ? (
+        <div className="animate-emoji-cascade flex flex-wrap gap-4">
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+          <span aria-label="Emoji" role="img">
+            🤫
+          </span>
+        </div>
+      ) : null}
+      {/* <div className="flex max-w-md flex-wrap gap-4">
+        <button
+          className="transition-color w-full rounded-lg bg-orange-500 p-4 text-xl font-medium hover:bg-orange-400"
+          type="button"
+          onClick={() =>
+            shareToInstagramStories(`${location.pathname}/opengraph-image`, "Pie de foto")
+          }
+        >
+          Instagram
+        </button>
+        <button
+          className="transition-color w-full rounded-lg bg-orange-500 p-4 text-xl font-medium hover:bg-orange-400"
+          type="button"
+          onClick={() =>
+            shareToInstagramStories(`${location.pathname}/opengraph-image`, "Pie de foto")
+          }
+        >
+          Facebook
+        </button>
+        <button
+          className="transition-color w-full rounded-lg bg-orange-500 p-4 text-xl font-medium hover:bg-orange-400"
+          type="button"
+          onClick={() =>
+            shareToInstagramStories(`${location.pathname}/opengraph-image`, "Pie de foto")
+          }
+        >
+          Twitter
+        </button>
+      </div> */}
+    </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+"use client";
 import type {Metadata} from "next";
 
+import React, {useState} from "react";
+import {useEffect} from "react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -8,10 +11,26 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    const loaderTimeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 3000); // Cambia este valor al número de milisegundos que quieras (3000 = 3 segundos)
+
+    return () => clearTimeout(loaderTimeout);
+  }, []);
+
   return (
     <html lang="en">
       <body>
-        <main className="m-auto min-h-screen max-w-screen-lg p-4 sm:p-24">{children}</main>
+        {showLoader ? (
+          <div className="loader loader-container">
+            <img alt="Loading" className="max-w-sm" src="/PreguntaXpress-loader-image.png" />
+          </div>
+        ) : (
+          <main className="m-auto min-h-screen max-w-screen-lg p-4 sm:p-24">{children}</main>
+        )}
       </body>
     </html>
   );
